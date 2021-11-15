588e43035e487cbba8412227b3eea2d4b193d033
Abhinav Dhiman <abhinav.dhiman@binmile.com>
2021-11-15 14:23:31 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementById("button").addEventListener("click",bmical);
+
+function bmical()
+{
+    const cm = parseInt(document.getElementById("cm").value);
+    const kg = parseFloat(document.getElementById("kg").value);
+
+    let bmi;
+    let newCm = parseFloat(cm/100);
+
+    bmi = kg / (newCm * newCm);
+    bmi = bmi.toFixed(1);
+    console.log(bmi);
+
+    document.getElementById("result").innerHTML = bmi;
+}
